193c081a4eebb5ca86f64579567fcdd2dfb703cc
MOMANI1 <mmn1078@gmail.com>
2016-04-11 09:08:09 +0300

    fix my problem when I added Hamzeh's modification

--- app/src/main/java/android/alcode/com/material/main/PostAdapter.java
+++ app/src/main/java/android/alcode/com/material/main/PostAdapter.java
@@ -9,6 +9,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -103,8 +106,26 @@ public class PostAdapter extends FirebaseRecyclerAdapter<PostDetails, RecyclerVi
     }
 
     @Override
-    public int getItemCount() {
-        return null == mPostList ? 0 : mPostList.size();
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder vh;
+        if (viewType == LAYOUT_SMALL) {
+            View v = mInflater.inflate(R.layout.layout_holder_movie_small, parent, false);
+            vh = new ViewHolderSmall(v);
+        } else {
+            View v = mInflater.inflate(R.layout.layout_holder_movie_large, parent, false);
+            vh = new ViewHolderLarge(v);
+        }
+        return vh;
+    }
+
+
+    private void setAnimation(View viewToAnimate, int position) {
+        // If the bound view wasn't previously displayed on screen, it's animated
+        if (position > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(mAct, android.R.anim.slide_out_right);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
     }
 
     @Override
